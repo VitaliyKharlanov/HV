d999a9340b75021e8962cc71f7766f906929547b
VitaliyKharlanov <neokom-tmn@mail.ru>
2021-12-09 23:57:27 +0500

    s

--- api/plus.php
+++ api/plus.php
@@ -15,7 +15,7 @@ $z = $x + $y;
 // $conn = mysqli_connect("localhost","root","","calc");
 // 4. Код уязвимый для SQL-injection
 
-include("C:\\AppParams/params.php");
+include(getenv('MYAPP_CONFIG'));
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 
 $sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonym')";
