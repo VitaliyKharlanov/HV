8e796e5dc406bc0256d0a72560f02887bea0ce33
VitaliyKharlanov <neokom-tmn@mail.ru>
2021-12-14 21:19:51 +0500

    cc

--- api/plus.php
+++ api/plus.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 // Это комментарий 
 
@@ -6,6 +7,7 @@
 $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x + $y;
+$u = $_SESSION["user"];
 
 
 // Здесь нарушены принципы безопасности:
@@ -18,7 +20,7 @@ $z = $x + $y;
 include(getenv('MYAPP_CONFIG'));
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 
-$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonym')";
+$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$u')";
 mysqli_query($conn,$sql);
 //echo(mysqli_error($conn));
 mysqli_close($conn);
